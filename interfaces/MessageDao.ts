e01e855f914c29debf3799af43f50baa489514e4
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-11-16 19:57:50 -0500

    Add interfaces for DAOs and Controllers

---
+++ interfaces/MessageDao.ts
@@ -0,0 +1,11 @@
+import Message from "../models/messages/Messages";
+
+/**
+ * @file Declares API for messages related data access object methods
+ */
+export default interface MessageDao {
+    createMessageByUsers(from: string, to: string, content: string): Promise<Message>;
+    deleteMessageById(mid: string): Promise<any>;
+    findAllMessagesSentByUser(uid: string): Promise<Message[]>;
+    findAllMessagesReceivedByUser(uid: string): Promise<Message[]>;
+};
