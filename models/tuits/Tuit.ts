18d07fdeb1ac3bfa9b449518d43cfa7fec82ea5c
wenhuang1 <huangwen@login.khoury.neu.edu>
2022-11-27 13:38:56 -0500

    Add a default img url for frontend devlopment

--- models/tuits/Tuit.ts
+++ models/tuits/Tuit.ts
@@ -10,7 +10,7 @@ export default interface Tuit {
     tuit: string,
     postedBy: User,
     postedOn?: Date,
-    image?: String,
+    image: "https://artistportfoliowebsite.s3.amazonaws.com/2022-08-31T23%3A27%3A52.877Z-R0000237.jpg",
     youtube?: String,
     avatarLogo?: String,
     imageOverlay?: String,
